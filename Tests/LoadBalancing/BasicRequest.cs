a8f79795cedb6fd9b9f17c5587c036d39158028d
Sander Saares <sasaares@microsoft.com>
2023-02-15 05:41:48 +0200

    Failure signaling

--- Tests/LoadBalancing/BasicRequest.cs
+++ Tests/LoadBalancing/BasicRequest.cs
@@ -69,6 +69,8 @@ internal sealed class BasicRequest : IRequest, IRoutedRequest
             FailureReason = reason;
 
             _notifyOnCompleted();
+
+            _resultsAggregator.OnRequestFailed();
         }
     }
 
