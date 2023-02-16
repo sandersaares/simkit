ad70856d1496cdc586508ac6569df41314cc0a33
Sander Saares <sasaares@microsoft.com>
2023-02-16 12:12:10 +0200

    Optimiz

--- Tests/LoadBalancing/BasicResultsAggregatorMetrics.cs
+++ Tests/LoadBalancing/BasicResultsAggregatorMetrics.cs
@@ -1,4 +1,5 @@
-﻿using Prometheus;
+﻿using System.Collections.Concurrent;
+using Prometheus;
 
 namespace Tests.LoadBalancing;
 
@@ -11,13 +12,20 @@ internal sealed class BasicResultsAggregatorMetrics
         RequestsCreated = metricFactory.CreateCounter("demo_requests_created_total", "Number of requests that have been created in the scenario.");
 
         _requestsFailedBase = metricFactory.CreateCounter("demo_requests_failed_total", "Number of requests that have failed for any reason, per reason.", new[] { "reason" });
+
+        _createRequestFailedByReasonDelegate = CreateRequestFailedByReason;
     }
 
     public Counter RequestsCompletedByClient { get; }
     public Counter RequestsCompletedByTarget { get; }
     public Counter RequestsCreated { get; }
 
-    public Counter.Child RequestsFailed(string reason) => _requestsFailedBase.WithLabels(reason);
+    public Counter.Child RequestsFailed(string reason) => _requestFailedByReason.GetOrAdd(reason, _createRequestFailedByReasonDelegate);
+
+    // reason -> counter
+    private readonly ConcurrentDictionary<string, Counter.Child> _requestFailedByReason = new();
+    private Counter.Child CreateRequestFailedByReason(string reason) => _requestsFailedBase.WithLabels(reason);
+    private Func<string, Counter.Child> _createRequestFailedByReasonDelegate;
 
     private readonly Counter _requestsFailedBase;
 }
--- Tests/LoadBalancing/RandomLoadBalancerMetrics.cs
+++ Tests/LoadBalancing/RandomLoadBalancerMetrics.cs
@@ -1,4 +1,5 @@
-﻿using Prometheus;
+﻿using System.Collections.Concurrent;
+using Prometheus;
 
 namespace Tests.LoadBalancing;
 
@@ -7,12 +8,16 @@ internal sealed class RandomLoadBalancerMetrics
     public RandomLoadBalancerMetrics(IMetricFactory metricFactory)
     {
         _routedRequestsBase = metricFactory.CreateCounter("demo_load_balancer_requests_routed_total", "Number of requests routed, by target.", new[] { "target" });
+
+        _createRoutedRequestByTargetDelegate = CreateRoutedRequestByTarget;
     }
 
     private readonly Counter _routedRequestsBase;
 
-    public Counter.Child RoutedRequests(string target)
-    {
-        return _routedRequestsBase.WithLabels(target);
-    }
+    public Counter.Child RoutedRequests(string target) => _routedRequestsByTarget.GetOrAdd(target, _createRoutedRequestByTargetDelegate);
+
+    // target ID -> counter
+    private readonly ConcurrentDictionary<string, Counter.Child> _routedRequestsByTarget = new();
+    private Counter.Child CreateRoutedRequestByTarget(string target) => _routedRequestsBase.WithLabels(target);
+    private Func<string, Counter.Child> _createRoutedRequestByTargetDelegate;
 }
